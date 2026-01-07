bdacbd2439909ac7d6d6a1521276bb5654e67622
Emilio Alejandro Ortiz Mijares <emilioortiz96@hotmail.com>
2026-01-06 20:33:55 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-01461673-795a-41d1-9de5-9b9c25ebbb76.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
